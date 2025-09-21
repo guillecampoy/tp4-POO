fa79e7d4369edddc95810dec8cd970cf2a7b9fac
Guillermo Campoy <guillecampoy@gmail.com>
2025-09-20 21:25:48 -0300

    Implemento creación de empleado simple Utilizando el constructor sobrecargado Se listan las opciones de aumentos para usuario simple porcentual y monto fijo

--- src/features/CreacionEmpleadoSimple.java
+++ src/features/CreacionEmpleadoSimple.java
@@ -1,10 +1,52 @@
 package features;
 
 import core.Ejecutable;
+import core.Empleado;
+import utils.ContextColor;
+import utils.UtilsColor;
+
+import java.util.Scanner;
 
 public class CreacionEmpleadoSimple implements Ejecutable {
     @Override
     public void execute() {
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Se procede a creación de empleado simple:");
+        Scanner scanner = new Scanner(System.in);
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese el Nombre del empleado: ",'n');
+        String nombreEmpleado = scanner.nextLine();
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese el puesto del empleado: ",'n');
+        String puestoEmpleado = scanner.nextLine();
 
+        Empleado empleado = new Empleado(nombreEmpleado, puestoEmpleado);
+
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Empleado cargado: "+empleado);
+        UtilsColor.imprimirBloque(ContextColor.INFO, "\nIncrementos salariales");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "1-Monto fijo");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, "2-Porcentaje");
+
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese su opción: ",'n');
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+        switch (opcion) {
+            case 1: {
+                UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese monto de incremento: ",'n');
+                double incrementoSalario = scanner.nextDouble();
+                scanner.nextLine();
+                empleado.incrementarSalarioMontoFijo(incrementoSalario);
+                break;
+            }
+            case 2: {
+                UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese porcentaje: ",'n');
+                double porcentajeSalario = scanner.nextDouble();
+                scanner.nextLine();
+                empleado.incrementarSalarioPorPorcentaje(porcentajeSalario);
+                break;
+            }
+            default: break;
+        }
+
+        UtilsColor.imprimirEncabezado();
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Estado Final empleado");
+        UtilsColor.imprimirBloque(ContextColor.DEFAULT, empleado.toString());
     }
 }
